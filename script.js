648c92e78e33caa651462667911c282d48042f07
chareeselam <chareeselam@gmail.com>
2026-02-04 01:05:45 -0500

    temp test for feedback

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const modalDialog = document.querySelector("#media-content");
 const closeButton = modalDialog.querySelector("button");
 
 grid.addEventListener("click", (e) => {
-  // finds the closest .tile that was clicked
-//   https://developer.mozilla.org/en-US/docs/Web/API/Event/target
+    // finds the closest .tile that was clicked
+    // https://developer.mozilla.org/en-US/docs/Web/API/Event/target
     const tile = e.target.closest(".tile");
 
     if (!tile) return;
+    // basicallty ignore click
     if (!tile.classList.contains("active")) return;
 
     modalDialog.showModal();
